38c28242bbe17356dd7ac08bcc8a2e52f7bf41fc
isc-ssl <stefan.sahlender@isc-ejpd.admin.ch>
2026-04-20 12:24:04 +0200

    updated script

--- push2gh.sh
+++ push2gh.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xv
-cd $HOME/git/sharedlibs/personal-scripts
+cd $HOME/git/sharedlibs
 git subtree split --prefix=personal-scripts/ssl/ai -b github-ai
 git push -u  github-ai github-ai:main
